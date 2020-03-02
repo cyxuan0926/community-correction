8e4cd91805e3a28af6f051df5ea5f794bb785b53
cy0926 <840473997@qq.com>
2020-03-02 18:33:40 +0800

    refactor: 搭建基本框架

---
+++ vue.config.js
@@ -0,0 +1,47 @@
+'use strict'
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir)
+
+const isProduction = () => process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetsDir: 'static',
+  productionSourceMap: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/assets/styles/var.scss";`
+      }
+    }
+  },
+  chainWebpack(config) {
+    config.when(isProduction(), config => {
+      config.optimization.splitChunks({
+        chunks: 'all',
+        cacheGroups: {
+          elementUI: {
+            name: 'chunk-elementUI',
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/,
+            priority: 60
+          },
+          vendor: {
+            name: 'chunk-vendor',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 50,
+            chunks: 'initial'
+          },
+          components: {
+            name: 'chunk-components',
+            test: resolve('src/components'),
+            priority: 30,
+            minChunks: 3,
+            reuseExistingChunk: true
+          }
+        }
+      })
+
+      config.optimization.runtimeChunk('single')
+    })
+  }
+}
