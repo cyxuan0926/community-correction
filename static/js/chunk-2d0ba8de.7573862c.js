6b7e0ba0d72b54e1a47f724c9da674f1ae105a73
caiyang <caiyang@sinog2c.com>
2020-10-27 18:06:18 +0800

    deploy

---
+++ static/js/chunk-2d0ba8de.7573862c.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ba8de"],{3869:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"home_view"})},c=[],u={data:function(){return{}}},s=u,i=t("9ca4"),l=Object(i["a"])(s,a,c,!1,null,null,null);e["default"]=l.exports}}]);
